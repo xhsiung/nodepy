b19721c36f66e543ea976f82aae076543f37343a
xhsiung <xhsiung@gmail.com>
2020-05-29 17:10:30 +0800

    fix

--- py/main.py
+++ py/main.py
@@ -6,7 +6,7 @@ def main( jstr ):
 def main2( jstr ):
     import json
     import sys,os
-    sys.path.append( "%s/py"%(os.getcwd()))
+    sys.path.append( "%s/py"%os.getcwd())
     #add module py/mymodule.py
     import mymodule
     return json.dumps(  {"success": False} )
